0363a9124401b8d502e2ccec5ce3802c8dc146d8
Mashitha <mashitharazanarv@gmail.com>
2026-02-23 21:42:03 +0530

    Added personal-bio-card

---
+++ personal-bio-card.py
@@ -0,0 +1,19 @@
+# Personal Bio Card Program
+
+# Variables
+name = "Mashitha Razana R V"
+age = "21 years"
+course = "Python Programming"
+college = "SJB Institute of Technology"
+email = "mashitharazanarv@gmail.com"
+
+# Bio-card
+print("╔════════════════════════════════════════╗")
+print("║           STUDENT BIO CARD             ║")
+print("╔════════════════════════════════════════╗")
+print(f"║ Name    : {name:<29}║")
+print(f"║ Age     : {age:<29}║")
+print(f"║ Course  : {course:<29}║")
+print(f"║ College : {college:<29}║")
+print(f"║ Email   : {email:<29}║")
+print("╚════════════════════════════════════════╝")
