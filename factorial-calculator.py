b39a41e6e230056aeb9a99b75dec11264863e13b
Mashitha <mashitharazanarv@gmail.com>
2026-02-24 08:34:46 +0530

    Added factorial-calculator

---
+++ factorial-calculator.py
@@ -0,0 +1,20 @@
+n = int(input("Enter a number: "))
+
+if n < 0:
+    print("Factorial not defined for negative numbers")
+
+elif n == 0:
+    print("0! = 1")
+
+else:
+    fact = 1
+    print(f"{n}! = ", end="")
+
+    for i in range(n, 0, -1):
+        fact *= i
+        print(i, end="")
+
+        if i != 1:
+            print(" × ", end="")
+
+    print(" =", fact)
